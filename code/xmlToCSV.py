64e93e928b35163cf88593cf22d134478e535f9a
Logan Grim <logangrim@149-160-190-174.dhcp-bl.indiana.edu>
2018-02-18 21:00:17 -0500

    Updated xmlToCSV contructor to take boolean isRunningFile.

--- code/xmlToCSV.py
+++ code/xmlToCSV.py
@@ -8,7 +8,7 @@ from xml.dom import minidom
 
 class xmlToCSV:
 
-    def __init__(self, inputFile, outputFile):
+    def __init__(self, inputFile, outputFile, isRunningFile):
 
         ########################
         ## Instance Variables ##
@@ -19,6 +19,12 @@ class xmlToCSV:
 
         # save the inputFile name
         self.inputFile = inputFile
+
+        # save the boolean that represents whether or not the
+        # input file is a running file or a biking file
+        self.isRunningFile = isRunningFile
+
+        ########################
 
         # Parse the XML file
         self.parseXML()
@@ -36,8 +42,6 @@ class xmlToCSV:
         # get the xml info
         xmlInfo = minidom.parse(self.inputFile)
 
-        # Start saving lists of each tag type that we are interested in
-
         # Heart Rate Tags
         # Note these contain sub tags named "value"
         heartRateBPMs = xmlInfo.getElementsByTagName('HeartRateBpm')
@@ -48,8 +52,6 @@ class xmlToCSV:
         for tag in heartRateBPMs:
             heartRateValues.append(tag.getElementsByTagName("Value")[0].firstChild.nodeValue)
 
-        print(heartRateValues)
-
         # Speed tags
         speedTags = xmlInfo.getElementsByTagName('ns3:Speed')
 
@@ -59,31 +61,64 @@ class xmlToCSV:
         for tag in speedTags:
             speeds.append(tag.firstChild.nodeValue)
 
-        print(speeds)
+        # Only grab this data if it is a running file
+        if self.isRunningFile:
+            # Run cadence tags
+            runCadenceTags = xmlInfo.getElementsByTagName('ns3:RunCadence')
 
-        # Run cadence tags
-        runCadenceTags = xmlInfo.getElementsByTagName('ns3:RunCadence')
+            # Grab the numerical run cadence values and put them in a list
 
-        # Grab the numerical run cadence values and put them in a list
+            runCadences = []
 
-        runCadences = []
+            for tag in runCadenceTags:
+                runCadences.append(tag.firstChild.nodeValue)
 
-        for tag in runCadenceTags:
-            runCadences.append(tag.firstChild.nodeValue)
+        # Only grab this data if it is a biking file
+        if not self.isRunningFile:
 
-        print(runCadences)
+            # Bike Cadence Tags
+            bikeCadenceTags = xmlInfo.getElementsByTagName("Cadence")
+
+            # Grab the numerical bike cadence values and put them in a list
+            bikeCadences = []
+
+            for tag in bikeCadenceTags:
+                bikeCadences.append(tag.firstChild.nodeValue)
+
+            # Power tags
+            powerTags = xmlInfo.getElementsByTagName("ns3:Watts")
+
+            # Grab the numerical tag values and put them in a list
+            powers = []
+
+            for tag in powerTags:
+                powers.append(tag.firstChild.nodeValue)
+
 
         # Write the data to the output file
         # Each column is an attribute
         # The structure is as follows:
         #   Heart Rate BPM, Speed, Run Cadence
-        for i in range(len(heartRateValues)):
-            self.outputFile.write(heartRateValues[i] + ", " + speeds[i] + ", " + runCadences[i] + "\n" )
+        #   NOTE: The bike files have a different third column (bike cadence) and a 4th column (power) :
+        #   Heart Rate BPM, Speed, Bike Cadence, Power
 
+        # Handle the case where there is not an equal number of each attribute
+        if self.isRunningFile:
+            numberOfObjects = min(len(heartRateValues), len(speeds), len(runCadences))
+        else:
+            numberOfObjects = min(len(heartRateValues), len(speeds), len(bikeCadences), len(powers))
+
+        for i in range(numberOfObjects):
+            if self.isRunningFile:
+                self.outputFile.write(heartRateValues[i] + ", " + speeds[i] + ", " + runCadences[i] + "\n" )
+            else:
+                self.outputFile.write(heartRateValues[i] + ", " + speeds[i] + ", " + bikeCadences[i] + powers[i] + "\n")
 
 ##########
 ## Test ##
 ##########
 
-obj = xmlToCSV("MoorsFTP_run.txt", "outputFile.csv")
+obj = xmlToCSV("MoorsFTP_run.txt", "runningOutput.csv", True)
+
+obj1 = xmlToCSV("MooneyBikeFTP.txt", "bikingOutput.csv", False)
 
